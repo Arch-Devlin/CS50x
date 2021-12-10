4809257650e408a5acdf1424a4a808a4c6d910ba
Arch-Devlin <87244096+Arch-Devlin@users.noreply.github.com>
2021-12-10 16:14:56 +0000

    Create hello.c

---
+++ Week1/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //get user's name
+    string name = get_string("What's your name? \n");
+
+    // greet user
+    printf("hello, %s\n", name);
+}
